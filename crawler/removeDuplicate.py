0d4850903a7134c5a5e2602a04b775cde1e9b23e
Zzealer <55761806+Zzealer@users.noreply.github.com>
2020-02-24 22:25:46 +0000

    CHANGES TO REMOVEDUPS

--- crawler/removeDuplicate.py
+++ crawler/removeDuplicate.py
@@ -1,7 +1,48 @@
 import os
 from lxml import etree
 
-path = '/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/dataset/reuters'
+#YOur path
+path = '/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/dataset'
+files = []
+
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.xml' in file:
+            files.append(os.path.join(r, file))
+
+f=open('Articles.xml','w',encoding='UTF-8')
+f.writelines('<?xml version="1.0"?>\n')
+f.writelines('<DATA>\n')
+for path in files:
+    with open(path,'r',encoding='UTF-8') as fr:
+        for line in fr:
+            if line == '<?xml version="1.0"?>\n':
+                continue
+            if line == '<DATA>\n':
+                continue
+            if line == '</DATA>\n':
+                continue
+            f.writelines(line)
+f.writelines('</DATA>\n')
+f.close()
+
+
+f = '/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/Articles.xml'
+dom = etree.parse(f)
+xsl = etree.parse('/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/check.xsl')
+
+# TRANSFORM XML
+transform = etree.XSLT(xsl)
+result = transform(dom)
+
+# SAVE OUTPUT TO FILE
+with open('/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/test.xml', 'wb') as fi:
+    fi.write(result)
+
+
+'''
+#original verison
+path = '/media/jsir/Windows/Users/jiang/Desktop/ed HPC/ttds/project/crawler/dataset/'
 files = []
 for r, d, f in os.walk(path):
     for file in f:
@@ -19,5 +60,4 @@ for f in files:
 # SAVE OUTPUT TO FILE
     with open(f, 'wb') as fi:
         fi.write(result)
-
-
+'''
